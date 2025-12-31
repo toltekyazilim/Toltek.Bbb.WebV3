7258b8942766398d621d1c557d634d895dd1c012
Yavuz coşar <yavuzcosar@gmail.com>
2025-12-31 15:15:25 +0300

    update

---
+++ wwwroot/_content/Toltek.Bbb.UI/toltek.berry-razor.js
@@ -0,0 +1,7 @@
+window.toggleSidebar = function () {
+    document.body.classList.toggle('pc-sidebar-collapse');
+};
+
+window.toggleMobileSidebar = function () {
+    document.body.classList.toggle('pc-sidebar-open');
+};
