656c2473bf900ebeefd630ad3d6035c7e6b0e3a9
Yavuz coşar <yavuzcosar@gmail.com>
2026-01-06 02:46:36 +0300

    update

---
+++ wwwroot/_content/Toltek.Themes.Berry.UI/berry/1.0.0/js/berryello.js
@@ -0,0 +1,37 @@
+﻿// Dinamik içerik gösterme işlevi
+document.querySelector('.dynamic-select').addEventListener('change', function () {
+    var selectedValue = this.value;
+
+    // Tüm içerikleri gizle
+    document.querySelectorAll('.content').forEach(function (content) {
+        content.style.display = 'none';
+    });
+
+    // Seçilen içeriği göster
+    document.getElementById(selectedValue).style.display = 'block';
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    tinymce.init({
+        selector: 'textarea.tinymce-editor',  // Editör uygulanacak textarea
+        plugins: 'lists link image table code', // Kullanmak istediğiniz eklentiler
+        toolbar: 'undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image', // Araç çubuğu ayarları
+        menubar: false, // Menü çubuğu göstermek istemezseniz false yapabilirsiniz
+        height: 300 // Yükseklik ayarı
+    });
+});
+
+const uppy = Uppy.Core()
+    .use(Uppy.Dashboard, {
+        inline: true,
+        target: '#uppy',
+        height: 400,
+        width: 600,
+        note: 'Dosyalarınızı buraya sürükleyebilir veya yüklemek için tıklayabilirsiniz.'
+    })
+    .use(Uppy.XHRUpload, {
+        endpoint: '/upload', // Yükleme uç noktası
+        formData: true,
+        fieldName: 'files[]'
+    });
+
---
+++ wwwroot/_content/Toltek.Themes.Berry.UI/berry/1.0.0/js/uppyload.js
@@ -0,0 +1,7 @@
+﻿import Uppy from '@uppy/core';
+import Dashboard from '@uppy/dashboard';
+
+import '@uppy/core/dist/style.min.css';
+import '@uppy/dashboard/dist/style.min.css';
+
+new Uppy().use(Dashboard, { inline: true, target: '#uppy-dashboard' });
